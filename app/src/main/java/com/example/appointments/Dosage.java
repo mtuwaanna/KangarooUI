6ace482dee1daea1015d7678a4ec550c861e2330
lau-jr <laurentdeusdedith74@gmail.com>
2021-06-09 21:15:06 +0300

    initial commit

---
+++ app/src/main/java/com/example/appointments/Dosage.java
@@ -0,0 +1,14 @@
+package com.example.appointments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Dosage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dosage);
+    }
+}
